ec2a61eb1905e80d230417b35d634d4cf258b793
Blubberdique <59876805+Blubberdique@users.noreply.github.com>
2023-06-07 10:59:24 +0200

    app.vue refers to index.vue, index.vue now holds basic template

--- app.vue
+++ app.vue
@@ -1,33 +1,15 @@
 <template>
-  <SearchFormSteps />
-  <OrderDetails />
+  <MainPage />
 </template>
 
 <script>
 import 'bootstrap/dist/css/bootstrap.css';
 
-import { provide, ref } from 'vue';
-import SearchFormSteps from './components/searchformsteps.vue';
-import OrderDetails from './components/orderdetails.vue';
+import MainPage from './pages/index.vue';
 
 export default {
   components: {
-    SearchFormSteps,
-    OrderDetails,
-  },
-  setup() {
-    const fetchedData = ref(null);
-
-    // Assuming you have a method or logic to fetch the data
-    const fetchData = async () => {
-      // Fetch the data and assign it to fetchedData
-    };
-
-    provide('fetchedData', fetchedData);
-
-    return {
-      fetchData,
-    };
+    MainPage,
   },
 };
 </script>
--- components/searchformsteps.vue
+++ components/searchformsteps.vue
@@ -131,14 +131,6 @@
       </div>
     </div>
   </div>
-  <div class="footer border-top">
-    <div class="text-center">
-      <p class="footer-p">
-        &copy; {{ new Date().getFullYear() }} Mangohoutonline<br />Aan de
-        informatie op onze website kunnen geen rechten worden ontleend
-      </p>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -180,93 +172,6 @@ export default {
 </script>
 
 <style>
-/* Navbar */
-.navbar {
-  height: 115px;
-  z-index: 1;
-  padding-top: 0 !important;
-}
-
-.filler {
-  width: 195.95px;
-}
-
-.image-container {
-}
-
-.btn {
-  color: #fff;
-}
-
-.btn-shop {
-  border-radius: 0 !important;
-  right: 20px;
-  background-color: rgb(187, 137, 84);
-}
-
-.btn-shop:hover {
-  color: #fff;
-  background-color: rgb(183, 125, 81);
-}
-
-.header {
-  padding-bottom: 20px;
-  padding-left: 40px;
-  background: black;
-  position: relative;
-}
-
-/* Banner */
-.background-image {
-  height: 300px;
-  position: relative;
-  background-size: cover;
-  background-image: url(../images/banner.jpg);
-}
-
-.background-image::before {
-  content: '';
-  display: block;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-image: url('../images/banner.jpg'); /* Replace with the actual image path */
-  background-size: cover;
-  background-position: center;
-  z-index: -1;
-}
-
-.overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(
-    0,
-    0,
-    0,
-    0.5
-  ); /* Adjust the desired overlay color and opacity */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.centered-text {
-  text-align: center;
-  color: #ffffff; /* Adjust the desired text color */
-  font-family: sans-serif;
-  font-weight: bold;
-}
-
-.centered-image {
-  max-width: 100%;
-  max-height: 100%;
-}
-
 /* Body */
 
 .form-body {
@@ -327,16 +232,5 @@ export default {
 .info-box-subtitle {
   color: #777;
   font-size: 14px;
-}
-/* Footer */
-
-.footer {
-  margin-top: 15px;
-  font-size: 11px;
-  font-family: sans-serif;
-}
-
-.footer-p {
-  margin-top: 15px;
 }
 </style>
